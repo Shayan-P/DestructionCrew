006318318d8741eb305c6c3b8340dc61d9cac06c
shayan <shayanpardis82@gmail.com>
2021-04-03 22:17:53 +0430

    add Graph.py starting to refactor

---
+++ Shayan/AI/Algorithms/Graph.py
@@ -0,0 +1,20 @@
+from AI.Map import Cell
+
+
+class Graph:
+    def __init__(self):
+        pass
+
+    def add_edge(self, a: Cell, b: Cell, w: int):
+        pass
+
+    def add_vertex(self, a: Cell, w: int):
+        pass
+
+    def get_shortest_distance(self):
+        pass
+
+    def get_shortest_path(self):
+        pass
+
+    # use shortest path algorithms implemented on the other file
---
+++ Shayan/AI/Algorithms/ShortestPath.py
@@ -0,0 +1,1 @@
+# implement shortest path algorithms here
