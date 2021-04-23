2ce2f28768644f7216ce7c69fa7648908a22801f
shayan <shayanpardis82@gmail.com>
2021-04-23 14:23:01 +0430

    remove spy

--- AI/Worker.py
+++ AI/Worker.py
@@ -9,17 +9,8 @@ class Worker(BaseAnt):
     def __init__(self, game):
         super(Worker, self).__init__(game)
         self.movement = GrabAndReturn(self)
-        self.spy = False
 
     def choose_best_strategy(self):
-        # if there are a little unknown cells stop exploring todo
-        if self.grid.chat_box_reader.get_now_turn() >= 35 and self.game.alive_turn == 0 and random.random() <= 0.3:
-            self.spy = True
-        if self.spy and self.previous_strategy is not None and self.previous_strategy_object.is_not_good():
-            self.spy = False
-        if self.spy:
-            return DeepSafeExplore
-
         if self.previous_strategy is None:
             self.previous_strategy = GrabAndReturn
             self.previous_strategy_object = GrabAndReturn(self)
