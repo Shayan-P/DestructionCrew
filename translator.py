a70abf7064444a06312c8486d91c0a2d2d7be9bc
shayan <shayanpardis82@gmail.com>
2021-04-26 19:06:40 +0430

    add save_warrior

--- translator.py
+++ translator.py
@@ -3,11 +3,11 @@ from AI.ChatBox import message_text_translator
 
 
 PLAYER_DATA = [[], []]
+ok = [True, True]
 
 with open('../AIC21-Game/server/log.json') as f:
 	mp = json.loads(f.read())
 
-	ok = True
 	turn_count = 0
 	ret = ""
 
@@ -15,30 +15,27 @@ with open('../AIC21-Game/server/log.json') as f:
 		turn_count += 1
 		ret += f"\nTurn {turn_count}\n\n"
 
-		for sss, chat in [("important0", turn['important_chat_box_0']), ("important1", turn['important_chat_box_1']), ("trivial0", turn['trivial_chat_box_0']), ("trivial1", turn['trivial_chat_box_1'])]:
-			ret += f"chat box {sss}:\n"
-			for mes in chat:
-				try:
-					ret += message_text_translator(mes['text']) + "\n"
-				except Exception as e:
-					ok_team = False
-
 		for player, chat in [(0, turn['trivial_chat_box_0']), (1, turn['trivial_chat_box_1'])]:
+			ret += f"team {player}:\n"
 			arr = []
 			for mes in chat:
 				mmm = ""
 				try:
 					mmm += message_text_translator(mes['text']) + "\n"
+					ret += message_text_translator(mes['text']) + "\n"
 				except Exception as e:
 					mmm += mes['text']
+					ok[player] = False
 				arr.append(mmm)
 			PLAYER_DATA[player].append(arr)
 
 	with open('../AIC21-Game/server/chat_box.log', 'w') as f:
 		f.write(ret)
 
-	if not ok:
-		print("error")
+	if not ok[0]:
+		print("error in parsing player 0")
+	if not ok[1]:
+		print("error in parsing player 1")
 
 	p = int(input("give me player (0 or 1): "))
 	assert p == 0 or p == 1
