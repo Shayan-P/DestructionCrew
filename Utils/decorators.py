c9efc77b761b311436763d9a6c5367b03d2d2488
shayan <shayanpardis82@gmail.com>
2021-04-30 05:09:03 +0430

    decorator error

--- Utils/decorators.py
+++ Utils/decorators.py
@@ -2,9 +2,13 @@ from AI.Config import Config
 
 func_names_to_turn = {}
 func_names_to_answer = {}
+names_alert = set()
 
 
 def once_per_turn(func):
+	assert func.__name__ not in names_alert
+	names_alert.add(func.__name__)
+
 	def outer_func(*args, **kwargs):
 		if func.__name__ not in func_names_to_turn:
 			func_names_to_turn[func.__name__] = -1
